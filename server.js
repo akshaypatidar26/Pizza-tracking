120d62a08e27c620db8ec4a58ac912b1731f46b6
akshaypatidar2604@gmail.com <akshaypatidar2604@gmail.com>
2021-05-20 00:34:02 +0530

    pizza tracker

--- server.js
+++ server.js
@@ -5,16 +5,28 @@ const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 
 const PORT = process.env.PORT || 3300
+const mongoose = require('mongoose')
+
+// Database connection
+const url ='mongodb://localhost/pizza'
+mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection failed...')
+});
+
 
 app.use(express.static('public'))
-app.get('/',(req,res)=>{
-    res.render('home')
 
-})
 
 app.use(expressLayout)
 app.set('views',path.join(__dirname, "/resources/views"))
 app.set('view engine', 'ejs')
+
+require('./routes/web')(app)
+
 
 
 app.listen(PORT, () => {
